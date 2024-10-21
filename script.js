f5dd5ec93551520da1579191bd73260e1128d0b1
opdemorais <opdemorais.1965@gmail.com>
2024-10-21 17:53:52 -0300

    Add files via upload
    meu mais novo projeto

---
+++ script.js
@@ -0,0 +1,3 @@
+// script.js
+// Simples, neste caso não há scripts complexos, mas esse arquivo está aqui para futuras melhorias
+console.log("Pedro Pinturas - Landing Page carregada com sucesso!");
